e8e7a4af9047746f66b39b869f14457268c1d2ad
alston <2>
2019-05-20 09:48:03 -0400

    1

---
+++ src/Delimiters.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Delimiters {
+    private String openDel;
+    private String closeDel;
+
+    public Delimiters(String open, String close)
+    {
+        openDel = open;
+        closeDel = close;
+    }
+    public ArrayList<String> getDelimiters(String[] tokens)
+    {
+            
+
+
+    }
+    public boolean isBalanced(ArrayList<String> delimiters)
+    {
+
+
+    }
+}
--- src/Question1.java
+++ src/Question1.java
@@ -1,8 +1,39 @@
 public class Question1 {
 
-    private static boolean isLeapYear(int year)
+    private static boolean isLeapYear(int year) {
+
+    }
+    public static int numberOfLeapYears(int year1, int year2)
+    {
+        int count=0;
+        for (int i=year1; i <= year2; i++)
+        {
+            if(isLeapYear(i))
+            {
+                count++;
+            }
+        }
+        return count;
+
+    }
+    private static int firstDayOfYear(int year)
     {
 
-        
+
+
+
+    }
+    public static int dayOfYear(int month, int day, int year)
+    {
+
+
+
+
+    }
+    public static int dayOfWeek(int month, int day, int year)
+    {
+
+
+
     }
 }
